42deceb21142fcb985aa8b77c83af4abcd6376ec
aditya pimpalkar <adipimpalkar@gmail.com>
2023-10-01 23:28:38 +0100

    getting started

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 function App() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_MAPS_API ?? ''
+  })
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='min-h-screen bg-slate-900 flex flex-col'>
+      <div>
+        {isLoaded && (
+          <GoogleMap
+            mapContainerStyle={{ height: '500px', width: '500px' }}
+            center={center}
+            zoom={10}
+          >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <></>
+          </GoogleMap>
+        )}
+      </div>
     </div>
   );
 }
